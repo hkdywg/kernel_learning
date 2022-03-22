01e338f6b27f49d8ea7941067c54f5664a80ed00
hkdywg <hkdywg@163.com>
2022-03-22 02:26:54 -0700

    Add: file transform for DLP encryption system
    Signed-off-by: hkdywg <hkdywg@163.com>

---
+++ Languge/shell/file_transform/README.md
@@ -0,0 +1,6 @@
+# 使用方法
+
+- 在装有DLP加密系统的电脑上运行file_trans_windows.py
+  运行成功后会生成output文件夹，将此文件夹拷贝到U盘中
+- 在linux系统中运行file_trans_ubuntu.py
+  运行成功后会生成output文件夹，此文件夹中的代码是可以直接阅读的
---
+++ Languge/shell/file_transform/file_trans_windows.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+
+output_dir = "output"
+
+def file_trans(filename):
+    # 打开一个文件
+    file_read = open(filename, "rb")
+    file_name_r = os.path.splitext(filename)[0]
+    #print(file_name_r)
+    file_name_r_suffix = os.path.splitext(filename)[-1]
+    if file_name_r_suffix == ".c" :
+        file_name_w = output_dir + file_name_r + ".cc"
+    elif file_name_r_suffix == ".h":
+        file_name_w = output_dir + file_name_r + ".hh"
+    elif file_name_r_suffix == ".cc":
+        file_name_w = output_dir + file_name_r + ".c"
+    elif file_name_r_suffix == ".hh":
+        file_name_w = output_dir + file_name_r + ".h"
+    else:
+        return
+    str = file_read.read()
+    file_write = open(file_name_w, "wb")
+    file_write.write(str)
+    file_read.close()
+    file_write.close()
+    return
+
+
+def list_filepath(file_path):
+    files = os.listdir(file_path)
+    for file in files:
+        file_d = os.path.join(file_path, file)
+        if os.path.isdir(file_d):
+            if file == output_dir:
+                print("ignore dir")
+            else:
+                os.mkdir(output_dir + file_d)
+                list_filepath(file_d)
+        else:
+            file_trans(file_d)
+    return
+
+
+if os.path.exists(output_dir):
+    print("outdir is exit")
+    list_filepath(".")
+else:
+    os.mkdir(output_dir)
+    list_filepath(".")
+
